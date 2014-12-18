0a4a9ceb26e9f30e6a8f394fd6a77bcff0bf14dd
max.snow <max.snow@4mation.com.au>
2014-12-19 10:42:57 +1100

    Migrations

---
+++ src/SCM/MySQLe/Database/Migrations/Migration.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration as IMigration;
+
+class Migration extends IMigration {
+
+    /**
+     * @param $tableName
+     * @param $fieldName
+     */
+    public function createDateTimeTrigger($tableName, $fieldName) {
+        DB::raw('DELIMITER $$');
+        DB::statement('DROP TRIGGER IF EXISTS `%s_BINS`', array($tableName));
+        DB::statement('CREATE TRIGGER `%s_BINS` BEFORE INSERT ON `%s` FOR EACH ROW SET NEW.`%s` = NOW()', array($tableName, $tableName, $fieldName));
+        DB::raw('DELIMITER ;');
+    }
+
+}
