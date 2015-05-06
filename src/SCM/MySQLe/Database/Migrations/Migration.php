f57d185db90e88ef4d39a9ae631f6c05f45c3a22
max.snow <max.snow@4mation.com.au>
2015-05-06 18:38:52 +1000

    Added the possibility to provide an array of fields to createDateTimeTrigger

--- src/SCM/MySQLe/Database/Migrations/Migration.php
+++ src/SCM/MySQLe/Database/Migrations/Migration.php
@@ -3,16 +3,27 @@
 class Migration extends \Illuminate\Database\Migrations\Migration {
 
     /**
+     * Create a trigger to set the date for every field provides to now, on record creation
+     *
      * @param $tableName
-     * @param $fieldName
+     * @param array $fieldNames
      */
-    public function createDateTimeTrigger($tableName, $fieldName) {
+    public function createDateTimeTrigger($tableName, $fieldNames) {
+        if (!is_array($fieldNames)) {
+            $fieldNames = [$fieldNames];
+        }
+
+        $fieldsString = '';
+        foreach ($fieldNames as $fieldName) {
+            $fieldsString .= sprintf('SET NEW.`%s` = NOW(); ', $fieldName);
+        }
+
         \DB::unprepared(sprintf('
             CREATE TRIGGER `%s_BINS` BEFORE INSERT ON `%s` FOR EACH ROW
             BEGIN
-            SET NEW.`%s` = NOW();
+            %s
             END
-        ', $tableName, $tableName, $fieldName));
+        ', $tableName, $tableName, $fieldsString));
     }
-
 }
+
