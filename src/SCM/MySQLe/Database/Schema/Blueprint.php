ba14c717f96946a5abd0fa094da26645b60fe551
max.snow <max.snow@4mation.com.au>
2015-12-04 17:44:51 +1100

    added set data type, documentation

--- src/SCM/MySQLe/Database/Schema/Blueprint.php
+++ src/SCM/MySQLe/Database/Schema/Blueprint.php
@@ -13,7 +13,24 @@ class Blueprint extends \Illuminate\Database\Schema\Blueprint {
         return $this->addColumn('binary', $column, compact('length'));
     }
 
+    /**
+     * Create an 'updated_at' timestamp on the table
+     *
+     * @param $column
+     * @return mixed
+     */
     public function updatedTimeStamp($column) {
         return $this->addColumn('updatedTimeStamp', $column);
+    }
+
+    /**
+     * Create a set column on the table
+     *
+     * @param $column
+     * @param array $allowed
+     * @return mixed
+     */
+    public function set($column, array $allowed) {
+        return $this->addColumn('set', $column, compact('allowed'));
     }
 }
--- src/SCM/MySQLe/Database/Schema/Grammars/MysqlGrammar.php
+++ src/SCM/MySQLe/Database/Schema/Grammars/MysqlGrammar.php
@@ -15,8 +15,25 @@ class MysqlGrammar extends \Illuminate\Database\Schema\Grammars\MySqlGrammar {
         return "binary({$column->length})";
     }
 
+    /**
+     * Create an 'updated_at' timestamp
+     *
+     * @param Fluent $column
+     * @return string
+     */
     protected function typeUpdatedTimeStamp(Fluent $column) {
         $column['nullable'] = true;
         return "TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL DEFAULT NULL";
+    }
+
+    /**
+     * Create a Set data type
+     *
+     * @param Fluent $column
+     * @param array $elements
+     * @return string
+     */
+    protected function typeSet(Fluent $column) {
+        return "set('".implode("', '", $column->allowed)."')";
     }
 }
